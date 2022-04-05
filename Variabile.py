e16fbc5dd47279f095057941598f0b41dd82367d
ralucah84 <ralucah84@gmail.com>
2022-04-05 20:37:16 +0300

    first commit

---
+++ Variabile.py
@@ -0,0 +1,25 @@
+# variabila = zona din memorie care tine date
+# variabila = cutiuta in care punem valori
+
+# am declarat si initializat variabila marca
+marca = 'Volvo'
+
+# am declarat si initializat variabila model
+model = 'XC 60'
+
+print(f'Am cumparat o masina marca: {marca}')
+print(f'Este modelul: {model}')
+
+#suprascriere
+model = 'XC 60 Facelift'
+
+print(f'Am cumparat o masina marca: {marca}')
+print(f'Este modelul: {model}')
+
+nume = 'Han'
+prenume = 'Raluca'
+varsta = '37'
+
+print (f'{prenume} {nume} {varsta}')
+
+print (len(marca))
---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# comentariu one line
+
+'''
+multiple comment
+linia 2
+linia 3
+'''
+
+# printam un mesaj in consola
+print ('hello world!')
